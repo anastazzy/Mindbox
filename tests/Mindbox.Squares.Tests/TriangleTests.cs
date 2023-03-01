1dfedb27fa79d13617a6bb3bb7b3605696d3d4af
Anastasiia <shitovanap@gmail.com>
2023-03-01 16:16:30 +0300

    fixed the tests

--- tests/Mindbox.Squares.Tests/TriangleTests.cs
+++ tests/Mindbox.Squares.Tests/TriangleTests.cs
@@ -13,20 +13,25 @@ public class TriangleTests : WithLibraryTest
     }
     
     [Fact]
-    public void GetSquare_TriangleIsNotRight_ReturnSquare()
+    public void GetSquare_TriangleCanNotExist_ReturnSquare()
     {
         var figure = Factory.CreateObject(10, 20, 13);
         Assert.Equal(56.15 , Math.Round(figure.GetSquare(), 2));
+    }
+    
+    [Fact]
+    public void GetSquare_TriangleIsNotRight_IsRightTriangleFalse()
+    {
+        var figure = Factory.CreateObject(10, 20, 13);
         
         var triangle = figure as ITriangle;
         Assert.False(triangle?.IsRightTriangle);
     }
     
     [Fact]
-    public void GetSquare_TriangleIsRight_ReturnSquare()
+    public void GetSquare_TriangleIsRight_IsRightTriangleTrue()
     {
         var figure = Factory.CreateObject(3, 4, 5);
-        Assert.Equal(6, Math.Round(figure.GetSquare(), 2));
         
         var triangle = figure as ITriangle;
         Assert.True(triangle?.IsRightTriangle);
